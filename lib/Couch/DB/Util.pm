6e7657b93b7c8b0abbe3421c38545c0bcf804c84
Mark Overmeer <mark@overmeer.net>
2024-06-03 10:20:12 +0200

    add pile apply_tree simplified

--- lib/Couch/DB/Util.pm
+++ lib/Couch/DB/Util.pm
@@ -8,8 +8,10 @@ use warnings;
 use strict;
 
 use Log::Report 'couch-db';
+use Data::Dumper ();
+use Scalar::Util qw(blessed);
 
-our @EXPORT_OK = qw/flat/;
+our @EXPORT_OK = qw/flat pile apply_tree simplified/;
 
 sub import
 {	my $class  = shift;
@@ -39,5 +41,50 @@ LIST may contain ARRAYs.
 =cut
 
 sub flat(@) { grep defined, map +(ref eq 'ARRAY' ? @$_ : $_), @_ }
+
+=function pile LIST|ARRAY
+Create a new ARRAY from the offered arguments, combining all elements
+from the LIST and ARRAYs.  Undefined elements are removed.
+=cut
+
+sub pile(@) { +[ flat @_ ] }
+
+=function apply_tree $tree, CODE
+Apply the CODE to all elements in the $tree.  Returns a new tree.
+=cut
+
+#XXX why can't I find a CPAN module which does this?
+
+sub apply_tree($$);
+sub apply_tree($$)
+{	my ($tree, $code) = @_;
+	    ! ref $tree          ? $code->($tree)
+	  : ref $tree eq 'ARRAY' ? +[ map apply_tree($_, $code), @$tree ]
+	  : ref $tree eq 'HASH'  ? +{ map +($_ => apply_tree($tree->{$_}, $code)), keys %$tree }
+	  : ref $tree eq 'CODE'  ? "$tree"
+	  :                        $code->($tree);
+}
+
+=function simplified $name, $data
+Returns a M<Data::Dumper> output, which is a simplified version of the $data.
+A normal dump would show internals of objects which make the output very verbose,
+hence harder to interpret.
+=cut
+
+sub simplified($$)
+{	my ($name, $data) = @_;
+
+	my $v = apply_tree $data, sub ($) {
+		my $e = shift;
+		    ! blessed $e         ? $e
+		  : $e->isa('DateTime')  ? "DATEtIME($e)"
+		  : $e->isa('Couch::DB::Document') ? 'DOCUMENT('.$e->id.')'
+		  : $e->isa('JSON::PP::Boolean')   ? ($e ? 'BOOL(true)' : 'BOOL(false)')
+		  : $e->isa('version')   ? "VERSION($e)"
+		  : 'OBJECT('.(ref $e).')';
+	};
+
+	Data::Dumper->new([$v], [$name])->Indent(1) ->Quotekeys(0)->Sortkeys(1)->Dump;
+}
 
 1;
